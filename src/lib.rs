dbf476f89a9fb0fdc83be46a30e97501232374ca
袁铭潮 <yuanmingchao@bilibili.com>
2024-05-18 03:24:04 +0800

    0517

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 use aws_config::{SdkConfig,Region};
-use aws_sdk_s3::config::{Credentials,SharedCredentialsProvider};
-// use aws_sdk_s3::types::Error;
+use aws_sdk_s3::{config::{Credentials,SharedCredentialsProvider}, error::SdkError, operation::{delete_object::{DeleteObjectError, DeleteObjectOutput}, head_object::{HeadObjectError, HeadObjectOutput}, put_object::{PutObjectError, PutObjectOutput}}, primitives::ByteStream};
+use core::str;
+// use tokio::time::error::Error;
+use std::{f32::consts::E, fmt::Error, fs::File, io::Write, path::{Path,PathBuf}, process::Output};
+// use aws_sdk_s3::types::Error as S3Error;
+
+pub struct PathPair {
+    pub oss_path: String,
+    pub local_path: String,
+}
 
 pub struct OssConfig {
     pub access_key: String,
@@ -35,42 +43,121 @@ impl OssConfig {
             client,
         }
     }
+}
 
-    pub async fn list_objects(&self, prefix: &str){
-        println!("Listing objects in bucket {}...", &self.bucket);
-        let mut response = self.client
+pub struct OssClient {
+    pub config: OssConfig,
+}
+
+impl OssClient {
+    pub fn new(access_key: String, secret_key: String, endpoint: String, region: String, bucket: String) -> OssClient {
+        OssClient {
+            config: OssConfig::new(access_key, secret_key, endpoint, region, bucket),
+        }
+    }
+
+    // 获取指定前缀的对象列表
+    pub async fn list_objects(&self, prefix: &str) -> Vec<String>{
+        println!("Listing objects in bucket {}...", &self.config.bucket);
+        let mut response = self.config.client
             .list_objects_v2()
-            .bucket(&self.bucket)
+            .bucket(&self.config.bucket)
             .max_keys(100)
             .prefix(prefix)
             .into_paginator()
             .send();
 
+        let mut objects = Vec::new();
         while let Some(result) = response.next().await {
             match result {
                 Ok(output) => {
+                    // return output;
                     for object in output.contents() {
-                        println!("{}", object.key().unwrap_or("Unknown"));
+                        let object_key = object.key().unwrap_or("Unknown");
+                        objects.push(object_key.to_string());
+                        // println!("{}", object_key);
                     }
                 }
                 Err(err) => {
-                    eprintln!("{err:?}")
+                    eprintln!("{err:?}");
                 }
             }
         }
+        objects
     }
 
-    // pub async fn download_object(&self, key: &str, local_path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    //     let data: GetObjectOutput = s3_service::download_object(&self.client, &self.bucket, &key).await?;
-    //     let data_length: u64 = data
-    //         .content_length()
-    //         .unwrap_or_default()
-    //         .try_into()
-    //         .unwrap();
-    //     if file.metadata().unwrap().len() == data_length {
-    //         println!("Data lengths match.");
-    //     } else {
-    //         println!("The data was not the same size!");
-    //     }
-    // }
+    // 下载单个文件到本地
+    pub async fn download_object(&self, path_pair: PathPair) -> Result<PathPair, Error>{
+        // 检查本地路径并创建文件夹
+        self.create_local_dir(path_pair.local_path.rsplitn(2, "/").last().unwrap()).unwrap();
+        let mut object = self.config.client
+            .get_object()
+            .bucket(&self.config.bucket)
+            .key(&path_pair.oss_path)
+            .send()
+            .await
+            .unwrap();
+
+        let mut file = std::fs::File::create(&path_pair.local_path).unwrap();
+        while let Some(bytes) = object.body.try_next().await.unwrap() {
+            file.write_all(&bytes).unwrap();
+        }
+        println!("Downloaded {} to {}", &path_pair.oss_path, &path_pair.local_path);
+        Ok(path_pair)
+    }
+
+    // 上传单个文件到OSS
+    pub async fn upload_object(&self, path_pair: PathPair) -> Result<PutObjectOutput, SdkError<PutObjectError>> {
+        let body = ByteStream::from_path(path_pair.local_path).await;
+        self.config.client
+            .put_object()
+            .bucket(&self.config.bucket)
+            .key(path_pair.oss_path)
+            .body(body.unwrap())
+            .send()
+            .await
+    }
+
+    // 删除OSS上的单个文件
+    pub async fn delete_object(&self, key: &str) -> Result<DeleteObjectOutput, SdkError<DeleteObjectError>>{
+        self.config.client
+                .delete_object()
+                .bucket(&self.config.bucket)
+                .key(key)
+                .send()
+                .await
+    }
+
+    // OSS文件检查
+    pub async fn check_object(&self, key: &str)-> Result<HeadObjectOutput, SdkError<HeadObjectError>> {
+        self.config.client
+            .head_object()
+            .bucket(&self.config.bucket)
+            .key(key)
+            .send()
+            .await
+    }
+
+    // 本地文件检查
+    pub fn check_local_file(&self, path: &str) -> bool {
+        Path::new(path).exists()
+    }
+
+    // 根据本地路径创建本地文件夹
+    pub fn create_local_dir(&self, path: &str) -> Result<(), std::io::Error> {
+        std::fs::create_dir_all(path)
+    }
+
+    // 获取文件夹中所有文件的路径
+    pub fn get_all_files_in_dir(&self, path: &str) -> Vec<PathBuf> {
+        let mut files = Vec::new();
+        for entry in std::fs::read_dir(path).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.is_file() {
+                files.push(path);
+            }
+        }
+        files
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use rs_oss_cli::*;
 
 #[tokio::main]
-async fn main() -> () {
-    let config = OssConfig::new(
+async fn main() {
+    let client = OssClient::new(
         "T90UlXgHTqGfovOh".to_string(),
         "tPIA6vHV7Jxkpmr4Ria75pj773AqDAZI".to_string(),
         "http://jssz-inner-boss.bilibili.co".to_string(),
         "jssz-inner".to_string(),
         "llm_snapshot".to_string());
 
-    config.list_objects("archimedes/eval").await;
+    // let objects_list = client.list_objects("archimedes/eval").await;
+    let key = "archimedes/configs/baichuan2-13B/config.json";
+    // let res = client.delete_object(key).await;
     // println!("{:?}", res);
+    let local_path = "./test/1/2/config.json";
+    let path_pair = PathPair {
+        oss_path: key.to_string(),
+        local_path: local_path.to_string(),
+    };
+    // let file_check = client.upload_object(path_pair).await;
+    let _ = client.download_object(path_pair).await;
+    // println!("{:?}", file_check);
 }

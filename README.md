dd90d78b2624d7078b7b57f98ca5c4adb0cd8929
袁铭潮 <yuanmingchao@bilibili.com>
2024-06-03 17:45:19 +0800

    v0.1.2

--- README.md
+++ README.md
@@ -3,16 +3,14 @@
 
 参数解释：
 
--action(-a) 包括upload,download,delete,list
+--action(-a) 包括upload,download,delete,list
 
--enable-dir(-e) 是否启动dir模式,dir模式默认输入输出路径均为dir(目前版本未实现强校验)
+--enable-dir(-e) 是否启动dir模式,dir模式默认输入输出路径均为dir(目前版本未实现强校验)
 
--local-path(-l) 本地路径
+--local-path(-l) 本地路径
 
--oss-path(-o) oss端路径
+--oss-path(-o) oss端路径
 
--config-path 配置文件路径(目前仅支持yaml)
+--config-path 配置文件路径(目前仅支持yaml), 支持从环境变量读取，命令行传递优先级高于环境变量
 
-    todo:实现从环境变量读取路径
-
--config-name 支持配置文件中配置项的选择(默认为default)
+--config-name 支持配置文件中配置项的选择(默认为default)，同上
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use rs_oss_cli::*;
 use clap::Parser;
 
@@ -21,18 +22,41 @@ struct Args {
     oss_path: String,
 
     /// 配置文件路径
-    #[arg(long, default_value = "./config.yaml")]
+    #[arg(long, default_value = "")]
     config_path: String,
 
     /// 配置选项
-    #[arg(long, default_value = "default")]
+    #[arg(long, default_value = "")]
     config_name: String,
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    let client = OssClient::new_from_file(&args.config_path, &args.config_name);
+    // 从环境变量中获取配置文件路径和配置选项，如果没有则使用命令行参数，环境变量优先级最高
+    let mut config_path = env::var("OSS_CONFIG_PATH")
+        .unwrap_or("".to_string());
+    let mut config_name: String = env::var("OSS_CONFIG_NAME")
+        .unwrap_or("".to_string());
+    if args.config_path.is_empty() {
+        if config_path.is_empty() {
+            config_path = "./config.yaml".to_string();
+        }
+        println!("Using config file: {}", config_path);
+    } else {
+        config_path = args.config_path.clone();
+        println!("Using config file: {}", config_path);
+    }
+    if args.config_name.is_empty() {
+        if config_name.is_empty() {
+            config_name = "default".to_string();
+        }
+        println!("Using config name: {}", args.config_name);
+    } else {
+        config_name = args.config_name.clone();
+        println!("Using config name: {}", config_name);
+    }
+    let client = OssClient::new_from_file(&config_path, &config_name);
     match args.action.as_str() {
         "upload" => {
             if args.enable_dir {
